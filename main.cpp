16b606700ee5049de2afafd256f996f5141f3e47
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-13 17:46:35 +0100

    Compressed files a bit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
 
-int main(int argc, char* argv[])
-{
-  if (argc == 1)
-  {
-    std::cout << argv[1] << '\n';
-  }
-  else
-  {
-    std::cout << "Too much commands" << '\n';
-  }
-
+int main() {
+  std::cout << "Hello world\n";
 }
